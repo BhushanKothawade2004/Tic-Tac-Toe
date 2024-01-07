42dfc239525f5240e6f2ea648b7f35988dfd9e02
BhushanKothawade2004 <bhushankothawade2004@gmail.com>
2024-01-08 02:37:49 +0530

    Added tie condition in code and rewritten JavaScript

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Tic Tac Toe (First Miniproject using Html, css, and JavaScript)
+
+# Added tie conditon in code and rewritten js
--- script.js
+++ script.js
@@ -1,86 +1,110 @@
 let boxes = document.querySelectorAll(".box");
-let resetBtn = document.querySelector("#reset-btn");
 let msgContainer = document.querySelector(".msgContainer");
+let winMsg = document.querySelector("#winner-msg");
 let newGameBtn = document.querySelector("#new-btn");
-let msg = document.querySelector("#winner-msg");
+let resetBtn = document.querySelector("#reset-btn");
 let tieContainer = document.querySelector(".tie-container");
-
-let turnX = true;
+let tieBtn = document.querySelector(".new-game")
 
 const winPatterns = [
     [0, 1, 2],
-    [0, 3, 6],
-    [0, 4, 8],
-    [1, 4, 7],
-    [2, 5, 8],
-    [2, 4, 6],
     [3, 4, 5],
     [6, 7, 8],
-]
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
+let turnX = true;
 
-let count = 0;
-
-boxes.forEach((box) => {
+for(let box of boxes){
     box.addEventListener("click", () => {
         if(turnX) {
             box.innerText = "X";
+            box.style.color = "#b0413e";
             turnX = false;
         } else {
-            box.innerText = "O"
+            box.innerText = "O";
+            box.style.color = "#556b2f";
             turnX = true;
-            box.style.color = "#556b2f"
         }
         box.disabled = true;
         box.style.backgroundColor = "rgba(255, 255, 199, 0.5)";
-        checkWinner();
-        count++;
-    })
-})
-
-const resetGame = () => {
-    turnX = true;
-    enableBoxes();
-    msgContainer.classList.add("hide");
-    for(box of boxes) {
-        box.style.backgroundColor = "white"
-    }
+        winGame();
+        tieCondition(); 
+    });
 }
 
+
 const disableBoxes = () => {
-    for (let box of boxes) {
+    for(let box of boxes) {
         box.disabled = true;
     }
 }
 
 const enableBoxes = () => {
-    for (let box of boxes) {
+    for(let box of boxes) {
         box.disabled = false;
-        box.innerText = "";
+        box.innerText = ""; 
+    }
+}
+
+const resetGame = () => {
+    turnX = true;
+    enableBoxes();
+    for(let box of boxes) {
+        box.style.backgroundColor = "white";
     }
 }
 
 const showWinner = (winner) => {
-    msg.innerText = `Congratulations Winner is ${winner}`;
-    let a = setTimeout(() => {           
-        msgContainer.classList.remove("hide");
-    },800);
+    winMsg.innerText = `Congrtaulations! Winner is ${winner}`
+    setTimeout (() => {
+        msgContainer.classList.remove("hide")
+    }, 500);
     disableBoxes();
 }
 
-const checkWinner = () => {
-    for(pattern of winPatterns){
-        pos1 = boxes[pattern[0]].innerText;
-        pos2 = boxes[pattern[1]].innerText;
-        pos3 = boxes[pattern[2]].innerText;
+const tieCondition = () => {
+    let winner = winGame();
+    let clickBox = 0;
+    for(let box of boxes) {
+        if(box.disabled) {
+            clickBox ++;
+        } 
+    }
+    if(clickBox === 9 && !winner) {
+        setTimeout(() => {
+            tieContainer.classList.remove("new-hide");
+        }, 500);
+    }
+}
 
-        if (pos1 != "" && pos2 != "" && pos3 != "") {
+const winGame = () => {
+    for(let idx of winPatterns) {
+        let pos1 = boxes[idx[0]].innerText;
+        let pos2 = boxes[idx[1]].innerText;
+        let pos3 = boxes[idx[2]].innerText;
+        if(pos1 != "" && pos2 != "" && pos3 != "") {
             if(pos1 === pos2 && pos2 === pos3) {
-                console.log("winer", pos1);
                 showWinner(pos1);
             }
         }
     }
 }
 
-newGameBtn.addEventListener("click", resetGame);
-resetBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", () => {
+    resetGame();
+});
+
+newGameBtn.addEventListener("click", () => {
+    msgContainer.classList.add("hide");
+    tieContainer.classList.add("new-hide");
+    resetGame();   
+});
+
+tieBtn.addEventListener("click", () => { 
+    tieContainer.classList.add("new-hide");
+    resetGame();
+})
